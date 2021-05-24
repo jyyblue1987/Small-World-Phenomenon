16d1de61a97c9ff0c8c4836caafa2507d80189a6
jyyblue <jyyblue1987@outlook.com>
2021-05-24 17:10:17 +0800

    [add] get selected investments

--- Knapsack/portfolio.py
+++ Knapsack/portfolio.py
@@ -19,24 +19,50 @@ def optimizeInvestments(investments, limit):
     n = len(investments)
 
     K = [[0 for x in range(limit + 1)] for x in range(n + 1)]
+    track = [[0 for x in range(limit + 1)] for x in range(n + 1)] 
 
     # Buiuld table K[][] in bottom up manner
     for i in range(n + 1):
         for w in range(limit + 1):            
-            if i == 0 or w == 0:
+            if i == 0 or w == 0: # the base cases, when we don’t select any investments, nomatter how much money we have to spend (top row), then we get no return on investment
                 K[i][w] = 0
+                track[i][w] = 0
             else:
                 wt = investments[i - 1][1]
-                if wt <= w:
-                    K[i][w] = max(investments[i - 1][2] + K[i - 1][w - wt], K[i - 1][w])
+                if w < wt:
+                    K[i][w] = K[i - 1][w]                    
                 else:
-                    K[i][w] = K[i - 1][w]
+                    val1 = investments[i - 1][2] + K[i - 1][w - wt]
+                    val2 = K[i - 1][w]
+                    if val1 > val2:
+                        K[i][w] = val1
+                        track[i][w] = 1 # select i th investment
+                    else:
+                        K[i][w] = val2                        
+                
+    # at the end to trace back through these winning options. 
+    nn = n
+    ww = w
+    sols = []
+    while nn > 1:
+        if track[nn][ww] == 1:
+            sols.append(nn - 1)            
+            ww = ww - investments[nn - 1][1]
+        
+        nn = nn - 1
 
-    return K[n][w], None
+    sols.reverse()
+
+    selected = []
+    for index in sols:
+        selected.append(investments[index][0])
+
+    
+    return K[n][w], selected  # goal location in the table,  lower-right corner
 
 investments = loadInvestments("input.txt")
 value, sol = optimizeInvestments(investments, 50)
 
-print(value)
+print(value, sol)
 
 
