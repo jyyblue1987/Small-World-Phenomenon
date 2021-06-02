928be451229936cc069c2ef0d04714d56ae5e2db
jyyblue <jyyblue1987@outlook.com>
2021-06-02 17:47:25 +0800

    [add] graph coloring backtracking.

--- lab_10/main.py
+++ lab_10/main.py
@@ -52,12 +52,59 @@ def nQueens(n):
                     if (currentState[previousCol] == currentRow) or abs(currentState[previousCol]-currentRow) == (currentCol - previousCol):
                         feasible = False
                         break
-                    if feasible:
-                        # Create child by making a copy and appending new col
-                        childState = currentState.copy()
-                        childState.append(currentRow)
-                        s.push(childState) # Push child onto data structure
+                if feasible:
+                    # Create child by making a copy and appending new col
+                    childState = currentState.copy()
+                    childState.append(currentRow)
+                    s.push(childState) # Push child onto data structure
 
 # Testing code (check 4,5,6,7)
 for n in range(4,8):
     nQueens(n)
+
+def graphColoring(graph, colors):
+    n = len(graph)
+    # Each state will include only the queens that have been placed so far
+    initialState = [] # Initial empty state
+
+    s = MyStack(list) # For a depth first search
+    s.push(initialState) # Push the initial state onto the Stack
+
+    # While we still have states to explore
+    while not s.empty():
+        currentState = s.pop() # Grab the next state
+        currentCol = len(currentState)
+        # See if we found a solved state at a leaf node
+        # That is, we have filled in every column with a queen
+        if currentCol == n:
+            print(currentState) # Display the solution
+            break
+        else:
+            # Produce the state's children (if they are feasible)
+            # Note children are produced backward so they come off the
+            # stack later left to right
+            for c in colors[::-1]:
+                # Check horizontal and both diagonals of previous queens
+                feasible = True
+                for previousCol in range(currentCol):
+                    # make sure they aren’t the same color as the one we are trying to color
+                    if (currentState[previousCol] == c) and graph[previousCol][currentCol]: # same color
+                        feasible = False
+                        break
+                if feasible:
+                    # Create child by making a copy and appending new col
+                    childState = currentState.copy()
+                    childState.append(c)
+                    s.push(childState) # Push child onto data structure
+
+# Adjacency matrix representation of a graph
+# This particular graph is the one from the videos
+graph = [[False, True, False, False, False, True ],
+        [True, False, True, False, False, True ],
+        [False, True, False, True, True, False],
+        [False, False, True, False, True, False],
+        [False, False, True, True, False, True ],
+        [True, True, False, False, True, False]]
+
+colors = ['r', 'g', 'b']
+graphColoring(graph, colors)
