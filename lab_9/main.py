0e279885956154216359aa9dff25eaa15a3b1daa
jyyblue <jyyblue1987@outlook.com>
2021-06-02 12:08:35 +0800

    [add] mst function.

--- lab_9/main.py
+++ lab_9/main.py
@@ -33,6 +33,40 @@ def shortest(g):
                     v[1] = u[1] + g[u[0]][v[0]]
     print(vertsProcessed)
 
+# Prims's MST algorithm
+def mst(g):
+    # Create a list of vertices and their current shortest distances
+    # from vertex 0
+    # [vertNum, dist]
+    nVerts = len(g)
+    vertsToProcess = [[i, float("inf")] for i in range(nVerts)]
+    parent = [None] * nVerts # Array to store constructed MST
+
+    # Start at vertex 0 - it has a current shortest distance of 0
+    vertsToProcess[0][1] = 0
+
+    tree = []
+    # Start with an empty list of processed edges
+    vertsProcessed = []
+
+    parent[0] = -1 # First node is always the root of
+
+    while len(vertsToProcess) > 0:
+        u = extractMin(vertsToProcess)
+        vertsProcessed.append(u)
+        tree.append([u[0], parent[u[0]]])
+        #print("to process:",vertsToProcess)
+        #print(" processed:",vertsProcessed)
+        # Examine all potential verts remaining
+        for v in vertsToProcess:
+            # Only care about the ones that are adjacent to u
+            if g[u[0]][v[0]] > 0:
+                # Update the distances if necessary
+                if g[u[0]][v[0]] < v[1]:
+                    v[1] = g[u[0]][v[0]]            
+                    parent[v[0]] = u[0]        
+    print(tree)
+
 # Adjacency matrix representation of a graph
 # This particular graph is the one from the videos
 # The vertices didn't have labels in the videos
@@ -52,5 +86,6 @@ graph = [[0, 7, 0, 0, 0, 10, 15, 0],
         [10, 0, 0, 8, 3, 0, 0, 0],
         [15, 0, 0, 0, 0, 0, 0, 0],
         [0, 9, 0, 0, 0, 0, 0, 0]]
-        
+
 shortest(graph)
+mst(graph)
